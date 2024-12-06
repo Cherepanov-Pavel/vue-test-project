ee46240f4854c8a48f8c14f2098ca0863eb3dac7
Pavel <pavel.cherepanov.98@gmail.com>
2024-12-07 02:58:32 +0500

    chore(eslint): install @eslint/json and update config for JSON validation

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import pluginVue from 'eslint-plugin-vue'
 import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import json from "@eslint/json";
 
 export default [
   {
@@ -14,4 +15,31 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
+
+	{
+		plugins: {
+			json,
+		},
+	},
+	{
+		files: ["**/*.json"],
+		language: "json/json",
+		rules: {
+			...json.configs.recommended.rules,
+		}
+	},
+	{
+		files: ["**/*.jsonc", ".vscode/*.json"],
+		language: "json/jsonc",
+		rules: {
+			...json.configs.recommended.rules,
+		}
+	},
+	{
+		files: ["**/*.json5"],
+		language: "json/json5",
+		rules: {
+			...json.configs.recommended.rules,
+		}
+	},
 ]
