014fdfd3a248dfe8b03a6f998cb650f2ceba38be
Pavel <pavel.cherepanov.98@gmail.com>
2024-12-07 18:02:13 +0500

    chore(eslint): install @stylistic/eslint-plugin and integrate into eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,54 +1,59 @@
 import json from "@eslint/json"
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import perfectionist from 'eslint-plugin-perfectionist'
-import pluginVue from 'eslint-plugin-vue'
+import stylistic from "@stylistic/eslint-plugin"
+import vueTsEslintConfig from "@vue/eslint-config-typescript"
+import perfectionist from "eslint-plugin-perfectionist"
+import pluginVue from "eslint-plugin-vue"
 
-const fixedVueConfig = pluginVue.configs['flat/essential']
-fixedVueConfig[2].files = [ '*.vue', '**/*.vue' ]
+const fixedVueConfig = pluginVue.configs["flat/essential"]
+fixedVueConfig[2].files = ["*.vue", "**/*.vue"]
 
 console.log(vueTsEslintConfig())
 export default [
   {
-    ignores: ['**/dist/**'],
+    ignores: ["**/dist/**"],
   },
   ...fixedVueConfig,
-	...vueTsEslintConfig(),
+  ...vueTsEslintConfig(),
 
   {
-		plugins: {
-			json,
-		},
-	},
-	{
-		files: ["**/*.json"],
-		language: "json/json",
-		rules: {
-			...json.configs.recommended.rules,
-		}
-	},
-	{
-		files: ["**/*.jsonc", ".vscode/*.json"],
-		language: "json/jsonc",
-		rules: {
-			...json.configs.recommended.rules,
-		}
-	},
-	{
-		files: ["**/*.json5"],
-		language: "json/json5",
-		rules: {
-			...json.configs.recommended.rules,
-		}
-	},
-
-	{
-		plugins: {
-      perfectionist,
-    },
-		rules: {
-      ...perfectionist.configs['recommended-natural'].rules,
-			'perfectionist/sort-interfaces': ['error', { partitionByNewLine: true, 'type': 'natural' }],
-      'perfectionist/sort-objects': ['error', { 'partitionByNewLine': true, 'type': 'natural' }],
+    plugins: {
+      json,
     },
   },
+  {
+    files: ["**/*.json"],
+    language: "json/json",
+    rules: {
+      ...json.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.jsonc", ".vscode/*.json"],
+    language: "json/jsonc",
+    rules: {
+      ...json.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.json5"],
+    language: "json/json5",
+    rules: {
+      ...json.configs.recommended.rules,
+    },
+  },
+
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      ...perfectionist.configs["recommended-natural"].rules,
+      "perfectionist/sort-interfaces": ["error", { partitionByNewLine: true, type: "natural" }],
+      "perfectionist/sort-objects": ["error", { partitionByNewLine: true, type: "natural" }],
+    },
+  },
+
+  stylistic.configs.customize({
+    quotes: "double",
+  }),
 ]
