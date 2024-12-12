86f24168cc278f0a8a20f28b0012bb92da6e5dda
Pavel <pavel.cherepanov.98@gmail.com>
2024-12-12 12:03:41 +0500

    chore: add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import {
 	fileURLToPath,
 	URL
 } from 'node:url'
+import {visualizer} from 'rollup-plugin-visualizer'
 import {defineConfig} from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
 	'plugins': [
 		vue(),
-		vueDevTools()
+		vueDevTools(),
+		visualizer({
+			'filename': 'node_modules/.tmp/stats.html',
+			'open': true
+		})
 	],
 	'resolve': {'alias': {'@': fileURLToPath(new URL('./src', import.meta.url))}},
 	'server': {'port': 53155}
